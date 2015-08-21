7acf8ae4156a8ddb5039eeda5f296300a5ca8c90
burakbaslican <burakbaslican@gmail.com>
2015-08-21 16:28:12 +0300

    Initial commit

---
+++ app/src/main/java/net/zahiri/materialmag/model/NavDrawerItem.java
@@ -0,0 +1,35 @@
+package net.zahiri.materialmag.model;
+
+/**
+ * Created by dhm17438 on 21.8.2015.
+ */
+public class NavDrawerItem {
+    private boolean showNotify;
+    private String title;
+
+
+    public NavDrawerItem() {
+
+    }
+
+    public NavDrawerItem(boolean showNotify, String title) {
+        this.showNotify = showNotify;
+        this.title = title;
+    }
+
+    public boolean isShowNotify() {
+        return showNotify;
+    }
+
+    public void setShowNotify(boolean showNotify) {
+        this.showNotify = showNotify;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
